71536b75ada6ffac1ef277056b96bc35aca9c1f5
atguigu <atguigu@atguigu.com>
2021-02-19 20:01:51 +0800

    second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("123");
+        // 切换版本命令： git reset --hard 版本号
+        System.out.println("版本2");
     }
 }
